27b8eeb2eb18e015454c3933721e697b3795ad22
derio123 <deriomatheus6@gmail.com>
2020-08-09 20:40:35 -0300

    Alterando caminho das imagens

--- src/app/List/mockheroes.ts
+++ src/app/List/mockheroes.ts
@@ -2,62 +2,62 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
 
-    {id: 1, name: 'Karla Sofen', heroname: 'MoonStone', breed:'Human', 
-    modifiers:'Moon Stones', profission:'Ph.D. in Psychology', level: 5,
-    img:'../assets/img/moonstone(karla)_head.jpg'},
-
-    {id: 2, name: 'Janet Van Dyne', heroname: 'Wasp', breed:'Human', 
-    modifiers:'Experiment', profission:'Scientist', level: 4, 
-    img:'../assets/img/waspy.jpg'},
-
-    {id: 3, name: 'Jennifer Susan Walters', heroname: 'SheHulk', breed:'Human', 
-    modifiers:'Experiment, Acident', profission:'Advocate', 
-    level: 6, img:'../assets/img/shehulk1.jpg'},
-
-    {id: 4, name: 'Carol Danvers', heroname: 'Captain Marvel', breed:'Kree-Human', 
-    modifiers:'Contact, Explosin', profission:'Pilot', level: 7, 
-    img:'../assets/img/captain-marvel_8.jpg'},
-    
-    {id: 5, name: 'Robert Reynolds', heroname: 'Sentry', breed:'Human', 
-    modifiers:'Super-Human; Experiment', profission:'', level: 9, 
-    img:'../assets/img/sentry.jpg'},
-    
-    {id: 6, name: 'Vision', heroname: 'Vision', breed:'Android', 
-    modifiers:'Vibranium; Infinity Stone', profission:'Pacifist', level: 8, 
-    img:'../assets/img/vision.jpg'},
-    
-    {id: 7, name: 'Lei Lung ', heroname: 'Air Lady', breed:'Human-Mutant', 
-    modifiers:'Fusion Ice, Experiment, Acindent', profission:'Architect', 
-    level: 6, img:'../assets/img/Aero_Lei_Ling.webp'},
-
-    {id: 8, name: 'Luna Show ', heroname: 'Ice Lady', breed:'Human-Mutant', 
-    modifiers:'Fusion Ice, Experiment, Acindent', profission:'dancer/singer', 
-    level: 6, img:'../assets/img/lunashow.JPG'},
-    
-    {id: 9, name: 'Aldrif Odinsttir ', heroname: 'Angela', breed:'Asgardin, Anjo', 
-    modifiers:'Enchanted', profission:'Queen of Asgard, The Hunter', level: 8, 
-    img:'../assets/img/Angela.webp'},
-
-    {id: 10, name: 'Peter Paker', heroname: 'Spider Man', breed:'Human', 
-    modifiers:'SuperSuit, Experiment', profission:'Student', level: 7, 
-    img:'../assets/img/spider-man-2.jpg' },
-
-    {id: 11, name: 'Sharon Rogers', heroname: 'Captain América', breed:'Human', 
+    {id: 1, name: 'Sharon Rogers', heroname: 'Captain América', breed:'Human', 
     modifiers:'Super Soldier', profission:'Soldier', level: 7, 
     img:'../assets/img/captainamerican_sharonrogers.jpg' },
 
-    {id: 12, name: 'Tony Stark', heroname: 'Iron Man', breed:'Human', 
+    {id: 2, name: 'Tony Stark', heroname: 'Iron Man', breed:'Human', 
     modifiers:'SuperSuit', profission:'Inventor', level: 7, 
-    img:'../assets/img/iron_man_vr.jpg' },                                  
+    img:'../assets/img/iron_man_vr.jpeg' },                                  
 
-    {id: 13, name: 'Wanda Maximoff', heroname: 'Scarlet Witch', breed:'Human', 
+    {id: 3, name: 'Wanda Maximoff', heroname: 'Scarlet Witch', breed:'Human', 
     modifiers:'Experiment', profission:'', level: 8, 
     img:'../assets/img/scarletwitch.jpg'},
 
-    {id: 14, name: 'Thor', heroname: 'Thor Odinson', breed:'Asgardin', 
+    {id: 4, name: 'Thor', heroname: 'Thor Odinson', breed:'Asgardin', 
     modifiers:'Enchanted', profission:'King of Asgard', level: 8, 
     img:'../assets/img/thor.jpg'},
 
+    {id: 5, name: 'Karla Sofen', heroname: 'MoonStone', breed:'Human', 
+    modifiers:'Moon Stones', profission:'Ph.D. in Psychology', level: 5,
+    img:'../assets/img/moonstone(karla)_head.jpg'},
+
+    {id: 6, name: 'Janet Van Dyne', heroname: 'Wasp', breed:'Human', 
+    modifiers:'Experiment', profission:'Scientist', level: 4, 
+    img:'../assets/img/waspy.jpg'},
+
+    {id: 7, name: 'Jennifer Susan Walters', heroname: 'SheHulk', breed:'Human', 
+    modifiers:'Experiment, Acident', profission:'Advocate', 
+    level: 6, img:'../assets/img/shehulk1.jpg'},
+
+    {id: 8, name: 'Carol Danvers', heroname: 'Captain Marvel', breed:'Kree-Human', 
+    modifiers:'Contact, Explosin', profission:'Pilot', level: 7, 
+    img:'../assets/img/captain-marvel_8.jpg'},
+    
+    {id: 9, name: 'Robert Reynolds', heroname: 'Sentry', breed:'Human', 
+    modifiers:'Super-Human; Experiment', profission:'', level: 9, 
+    img:'../assets/img/sentry.jpg'},
+    
+    {id: 10, name: 'Vision', heroname: 'Vision', breed:'Android', 
+    modifiers:'Vibranium; Infinity Stone', profission:'Pacifist', level: 8, 
+    img:'../assets/img/vision.jpg'},
+    
+    {id: 11, name: 'Lei Lung ', heroname: 'Air Lady', breed:'Human-Mutant', 
+    modifiers:'Fusion Ice, Experiment, Acindent', profission:'Architect', 
+    level: 6, img:'../assets/img/Aero_Lei_Ling.webp'},
+
+    {id: 12, name: 'Luna Show ', heroname: 'Ice Lady', breed:'Human-Mutant', 
+    modifiers:'Fusion Ice, Experiment, Acindent', profission:'dancer/singer', 
+    level: 6, img:'../assets/img/lunashow.JPG'},
+    
+    {id: 13, name: 'Aldrif Odinsttir ', heroname: 'Angela', breed:'Asgardin, Anjo', 
+    modifiers:'Enchanted', profission:'Queen of Asgard, The Hunter', level: 8, 
+    img:'../assets/img/Angela.webp'},
+
+    {id: 14, name: 'Peter Paker', heroname: 'Spider Man', breed:'Human', 
+    modifiers:'SuperSuit, Experiment', profission:'Student', level: 7, 
+    img:'../assets/img/spider-man-2.jpg' },
+    
     {id: 15, name: 'Bruce Banner', heroname: 'Hulk', breed:'Human', 
     modifiers:'Super Soldier; Experiment', profission:'Scientist', level: 6, 
     img:'../assets/img/hulk.jpg'},
